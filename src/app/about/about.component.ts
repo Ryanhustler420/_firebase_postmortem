becdbd86f78940cdfb314bd4886bfe13ff7eda7e
Gaurav Gupta <gouravgupta840@gmail.com>
2019-07-13 16:46:36 +0530

    The AngularFire API - snapshotChanges

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Course } from './../model/course';
 
 
 const config = {
@@ -23,7 +24,18 @@ export class AboutComponent implements OnInit {
   constructor(private db: AngularFirestore) { }
 
   ngOnInit() {
-    this.db.collection('courses').valueChanges().subscribe(val => console.log(val));
+    this.db.collection('courses')
+    .snapshotChanges()
+    .subscribe(snaps => {
+      const courses: Course[] = snaps.map(snap => {
+        return <Course> {
+          id: snap.payload.doc.id,
+          ...snap.payload.doc.data()
+        };
+      });
+
+      console.log(courses);
+    });
   }
 
 }
