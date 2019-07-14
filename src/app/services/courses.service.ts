d98def19e3a43d8164840e3e917a274a9e1a7cff
Gaurav Gupta <gouravgupta840@gmail.com>
2019-07-14 10:51:59 +0530

    The Edit Course Dialog

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -13,62 +13,18 @@ export class CoursesService {
 
   constructor(private db: AngularFirestore) { }
 
+  saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
+    return new Observable(null);
+  }
+
   loadAllCourses(): Observable<Course[]>  {
-    // return this.db.collection('courses', ref => ref.orderBy('seqNo')).snapshotChanges()
 
-    // return this.db.collection('courses', ref => ref.where('seqNo', '==', 2))
-
-    // return this.db.collection('courses', ref =>
-    //   ref
-    //     .where('seqNo', '>', 2)
-    //     .where('seqNo', '<=', 5)
-    // )
-
-    // this below is same as above code
-
-    // return this.db.collection('courses', ref =>
-    //   ref.orderBy('seqNo')
-    //   // .startAt(0).endAt(5)
-    //   .startAfter(0).endAt(5)
-    // )
-
-    // Array Filtering
-    // return this.db.collection('courses', ref =>
-    //   ref.where('categories', 'array-contains', 'BEGINNER')
-    // )
-
-    return this.db.collection('courses', ref =>
-      ref.orderBy('seqNo')
-        // this quere requires an inedx. so gives an error along with a link
-
-        // https://console.firebase.google.com/project/fir-scratch-board/database/firestore/indexes?create_
-        // composite=ClFwcm9qZWN0cy9maXItc2NyYXRjaC1ib2FyZC9kYXRhYmFzZXMvKGRlZmF1
-        // bHQpL2NvbGxlY3Rpb25Hcm91cHMvY291cnNlcy9pbmRleGVzL18QARoJCgVzZXFObxABGhAKDGxlc3NvbnNDb3VudBABGgwKCF9fbmFtZV9fEAE
-
-        // grab this link and create an index
-    )
-
-    // throw an error because its not possible by the indexing order
-    // return this.db.collection('courses', ref =>
-    //   ref
-    //     .where('seqNo', '>=', 5)
-    //     .where('lessonsCount', '>=', 10)
-
-    // this is also not valid. for this to work we have to make
-    // index lessionCount and seqNo instead of seqNo and lessionCount.
-    //     .where('seqNo', '>=', 5)
-    //     .where('lessonsCount', '==', 10)
-
-    // )
+    return this.db.collection('courses', ref => ref.orderBy('seqNo'))
 
     .snapshotChanges()
       .pipe(
-        map((snaps) => {
-          return convertSnaps<Course>(snaps);
-          // first will prevent real time data update.
-      }),
-        first(),
-      // take()
+        map(snaps => convertSnaps<Course>(snaps)),
+        first()
       );
   }
 
