8c619b36e592a3eb4839f19f2459ec9f1d0954cf
Gaurav Gupta <gouravgupta840@gmail.com>
2019-07-14 10:36:05 +0530

    Pagination and Loading Indicator

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,7 +1,7 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Course} from '../model/course';
-import {tap} from 'rxjs/operators';
+import {tap, finalize} from 'rxjs/operators';
 import {Observable} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import { CoursesService } from './../services/courses.service';
@@ -18,6 +18,10 @@ export class CourseComponent implements OnInit {
 
   lessons: Lesson[];
 
+  lastPageLoaded = 0;
+
+  loading = false;
+
   displayedColumns = ['seqNo', 'description', 'duration'];
 
 
@@ -30,14 +34,21 @@ export class CourseComponent implements OnInit {
 
   ngOnInit() {
 
+    this.loading = true;
+
     this.course = this.route.snapshot.data['course'];
 
     this.coursesService.findLessons(this.course.id)
+      .pipe(finalize(() => this.loading = false))
       .subscribe(lessons => this.lessons = lessons);
   }
 
   loadMore() {
-
+    this.loading = true;
+    this.lastPageLoaded++;
+    this.coursesService.findLessons(this.course.id, this.lastPageLoaded)
+    .pipe(finalize(() => this.loading = false))
+    .subscribe(lessons => this.lessons = this.lessons.concat(lessons));
   }
 
 
