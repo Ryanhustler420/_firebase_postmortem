c03a0d3664b847b750bf444b16fa1dc12cceba26
Gaurav Gupta <gouravgupta840@gmail.com>
2019-07-14 20:38:28 +0530

    Auth User Profile - Login / Logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+    isLoggedIn$: Observable<boolean>;
+    isLoggedOut$: Observable<boolean>;
+
+    constructor(private afAuth: AngularFireAuth) {}
+
+    ngOnInit() {
+      this.afAuth.authState.subscribe(user => console.log(user));
+      this.isLoggedIn$ = this.afAuth.authState.pipe(map(user => !!user));
+      this.isLoggedOut$ = this.isLoggedIn$.pipe(map(loggedIn => !loggedIn));
+    }
 
     logout() {
-
+      this.afAuth.auth.signOut();
     }
 
 }
